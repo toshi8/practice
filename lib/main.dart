ead19e0a63591699bd2012c3426fe5f1cfbf7f3e
Toshi <toshihiro.nakamura8@gmail.com>
2021-06-02 14:50:06 +0900

    use setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:practice/video_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,168 +27,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final items = List<String>.generate(14, (i) => "Item $i");
+  String thisText = 'Create model';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: false,
-        titleSpacing: 0.0,
-        title: Container(
-          width: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                child: Text(
-                  'youtube',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        leading: Icon(
-          Icons.video_call_rounded,
-          color: Colors.red,
-          size: 30.0,
-        ),
-        actions: <Widget>[
-          Icon(
-            Icons.search_sharp,
-            color: Colors.black,
-            size: 30.0,
-          ),
-          Icon(
-            Icons.more_vert,
-            color: Colors.black,
-            size: 30.0,
-          ),
-        ],
+        title: Text('Create model'),
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
-            return ListTile(
-              // title: Text('${items[index]}'),
-              title: Row(
-                children: <Widget>[
-                  Image.network(
-                    'https://s3.whisky-img.com/page_icon/medium/955.png',
-                    width: 55,
-                    height: 55,
-                  ),
-                  Column(
-                    children: [
-                      ConstrainedBox(
-                        constraints:
-                            BoxConstraints(maxWidth: 320.0, maxHeight: 64.0),
-                        child: Container(
-                          width: double.infinity,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              top: 8,
-                              left: 8,
-                            ),
-                            child: Text(
-                              'ウイスキー大学',
-                              style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        child: Row(
-                          children: [
-                            TextButton(
-                              onPressed: () {},
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.video_call_rounded,
-                                    color: Colors.red,
-                                  ),
-                                  Text(
-                                    'チャンネル登録',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              style: ButtonStyle(
-                                padding:
-                                    MaterialStateProperty.all(EdgeInsets.zero),
-                                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(2),
-                              child: Text(
-                                'チャンネル登録者数 76900人',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }
-          return ListTile(
-            leading: Image.network(
-              'https://s3.whisky-img.com/createThumbnail/medium/46.jpg',
-              width: 100,
-              height: 90,
+      body: Column(
+        children: [
+          Text(
+            thisText,
+            style: TextStyle(
+              fontSize: 15,
             ),
-            title: Text('Firebase Firestoreのセキュリティルール勉強会 by Kosuke'),
-            subtitle: Text('296 回視聴・2日前'),
-            trailing: IconButton(
-              icon: const Icon(Icons.more_vert),
-              onPressed: () {
-                print('onTapIcon');
-              },
-            ),
-            onTap: () => {
-              print('onTap'),
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => VideoPage()),
-              ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                thisText = 'new';
+              });
             },
-          );
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'ホーム',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_fire_department),
-            label: '急上昇',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt),
-            label: '登録チャンネル',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books),
-            label: 'ライブリラリ',
+            child: Text('push'),
           ),
         ],
-        type: BottomNavigationBarType.fixed,
       ),
     );
   }
